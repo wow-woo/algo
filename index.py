bf39967cbb324093ee9315b14e0c954ffcbad975
wow-woo <fwpass@outlook.com>
2020-12-23 18:10:24 +0900

    simply note

--- index.py
+++ index.py
@@ -1,19 +1,45 @@
-def moveElementToEnd(array, toMove):
-	last_idx=len(array)-1
-	idx=0
-	while idx <= last_idx and last_idx >=0:
-		if array[idx] == toMove:
-			#swap
-			#target to swap could be toMove
-			while array[last_idx] == toMove and idx < last_idx:
-				last_idx -= 1
-			
-			#you don't look back since No swap needed,
-			if idx >= last_idx:
-				break
-				
-			array[idx], array[last_idx] = array[last_idx], array[idx]
-			last_idx -= 1
-		idx += 1
-			
-	return array
+def traverseTree(tree, target, closest, n):
+    if not tree:
+        return
+    diff_l = n_l = diff_r = n_r = float('inf')
+
+    diff = abs(tree.value - target)
+    if diff < closest:
+        closest = diff
+        n = tree.value
+
+    if tree.value > target:
+        # make tree.value smaller
+        if tree.left:
+            diff_l, n_l = traverseTree(tree.left, target, closest, n)
+    elif target > tree.value:
+        # make tree.value bigger
+        if tree.right:
+            diff_r, n_r = traverseTree(tree.right, target, closest, n)
+    else:
+        return [diff, tree.value]
+
+    mi = min(diff, diff_l, diff_r)
+    if mi == diff:
+        return [diff, tree.value]
+    elif mi == diff_l:
+        return [diff_l, n_l]
+    elif mi == diff_r:
+        return [diff_r, n_r]
+
+
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+    closest = float('inf')
+    n = float('inf')
+
+    return traverseTree(tree, target, closest, n)[1]
+
+# This is the class of the input tree. Do not edit.
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
